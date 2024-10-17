befac33a152bf882c1d867a721a9eb00a72a7860
Thomas <141788769+thmsVDC@users.noreply.github.com>
2024-10-17 16:24:53 -0300

    Update register.py

--- app/customer/register.py
+++ app/customer/register.py
@@ -1,4 +1,4 @@
-from flask_wtf import FlaskForm
+aifrom flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import Length, EqualTo, Email, DataRequired, ValidationError
 from .model import Customer
@@ -8,12 +8,12 @@ class RegisterForm(FlaskForm):
     def validate_username(self, username):
         user = Customer.query.filter_by(username=username.data).first()
         if user:
-            raise ValidationError('Username already exists! Please try a different username.')
+            raise ValidationError('Usuário já cadastrado! Utilize outro.')
 
     def validate_email_address(self, email_address):
         email = Customer.query.filter_by(email_address=email_address.data).first()
         if email:
-            raise ValidationError('Email Address already exists! Please try a different email address.')
+            raise ValidationError('Email já cadastrado. Utilize outro.')
 
     username = StringField(label='User Name:', validators=[Length(min=2, max=30), DataRequired()])
     email_address = StringField(label='Email Address:', validators=[Email(), DataRequired()])
