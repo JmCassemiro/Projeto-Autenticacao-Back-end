094b9f16fe2175e1dcbfeb2705861b240d39a7c9
Thomas <141788769+thmsVDC@users.noreply.github.com>
2024-11-19 14:50:34 -0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -6,7 +6,7 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or "sqlite:///auth.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    SECRET_KEY = "ec9439cfc6c796ae2029594d"  # ou os.environ.get('SECRET_KEY') ou os.urandom(24)
+    SECRET_KEY = os.environ.get('SECRET_KEY') or "ec9439cfc6c796ae2029594d"
 
     HOME_TEMPLATE_FOLDER = os.path.join(BASE_DIR, "app/home/templates")
     CUSTOMER_TEMPLATE_FOLDER = os.path.join(BASE_DIR, "app/customer/templates")
