412659648a08e01f54e56c682d31329fc1d5ecee
thomasvictor <thomasvictor2909@gmail.com>
2024-11-19 15:08:59 -0300

    Alterando arquivo do banco de dados

--- utils/config.py
+++ utils/config.py
@@ -2,13 +2,14 @@ import os
 
 BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 
-
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or "sqlite:///auth.db"
+    DB_PATH = os.path.join(BASE_DIR, 'utils', 'authentication.db')
+    SQLALCHEMY_DATABASE_URI = f"sqlite:///{DB_PATH}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     SECRET_KEY = os.environ.get("SECRET_KEY") or "ec9439cfc6c796ae2029594d"
 
+    # Caminhos para os templates
     HOME_TEMPLATE_FOLDER = os.path.join(BASE_DIR, "app/home/templates")
     CUSTOMER_TEMPLATE_FOLDER = os.path.join(BASE_DIR, "app/customer/templates")
     COMMON_TEMPLATES_FOLDER = os.path.join(BASE_DIR, "app/common_templates")
